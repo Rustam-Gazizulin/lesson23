74e7e0b4d1a663722125d54f34344f408d8bfa9b
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-08-08 22:18:15 +0500

    work try_except

---
+++ try_except.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+class ApiError(Exception):
+    pass
+
+
+def division(a, b):
+    try:
+        a = int(a)
+        b = int(b)
+    except (ValueError, TypeError):
+        raise ApiError('a or b isn`t int')
+
+    try:
+        return a / b
+    except ZeroDivisionError:
+        raise ApiError('b is zero')
+
+
+@app.route('/')
+def hello_world():
+    a = request.args.get('a')
+    b = request.args.get('b')
+
+    try:
+        res = division(a, b)
+        return jsonify({'result': res})
+    except ApiError as e:
+        return jsonify({'error': str(e)})
+
+
+app.run()
